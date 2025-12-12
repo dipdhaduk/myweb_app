6e78555c7a45162df2248f3bc5a9a22a16564303
dipdhaduk <dhadukdipj@gmail.com>
2025-12-12 17:47:30 +0530

    Refactor navigation links in Home and Contact components to use React Router's Link for better routing. Fix minor typos and improve accessibility attributes.

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import React, { useState } from 'react'
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Contact() {
 
@@ -40,24 +40,24 @@ export default function Contact() {
   <div>
     <nav className="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
       <div className="container">
-        <a className="navbar-brand" href="index.html">Furni<span>.</span></a>
+        <Link className="navbar-brand" to="/">Furni<span>.</span></Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarsFurni">
           <ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
             <li className="nav-item">
-              <a className="nav-link" href="index.html">Home</a>
+              <Link className="nav-link" to="/">Home</Link>
             </li>
-            <li><a className="nav-link" href="shop.html">Shop</a></li>
-            <li><a className="nav-link" href="about.html">About us</a></li>
-            <li><a className="nav-link" href="services.html">Services</a></li>
-            <li><a className="nav-link" href="blog.html">Blog</a></li>
-            <li className="active"><a className="nav-link" href="contact.html">Contact us</a></li>
+            <li><Link className="nav-link" to="/shop">Shop</Link></li>
+            <li><Link className="nav-link" to="/about">About us</Link></li>
+            <li><Link className="nav-link" to="/service">Services</Link></li>
+            <li><a className="nav-link" href="#">Blog</a></li>
+            <li className="active"><Link className="nav-link" to="/contact">Contact us</Link></li>
           </ul>
           <ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-            <li><a className="nav-link" href="#"><img src="images/user.svg" /></a></li>
-            <li><a className="nav-link" href="cart.html"><img src="images/cart.svg" /></a></li>
+            <li><Link className="nav-link" to="/login"><img src="images/user.svg" /></Link></li>
+            <li><Link className="nav-link" to="/cart"><img src="images/cart.svg" /></Link></li>
           </ul>
         </div>
       </div>
@@ -71,7 +71,7 @@ export default function Contact() {
             <div className="intro-excerpt">
               <h1>Contact</h1>
               <p className="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.</p>
-              <p><a href className="btn btn-secondary me-2">Shop Now</a><a href="#" className="btn btn-white-outline">Explore</a></p>
+              <p><a href="#" className="btn btn-secondary me-2">Shop Now</a><a href="#" className="btn btn-white-outline">Explore</a></p>
             </div>
           </div>
           <div className="col-lg-7">
